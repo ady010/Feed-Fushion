327ff241a8b4814511d5c25286d856273507b701
Aditya Pahane <adityapahane@gmail.com>
2025-03-31 16:38:37 +0530

    Updating...

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express()
+const Routes = require("../src/Routes/index.routes")
+const cors = require("cors")
 
+app.use(cors({origin:"http://localhost:5173"},{Credantial:true}))
 
-
+app.use("/",Routes)
 
 module.exports = app
