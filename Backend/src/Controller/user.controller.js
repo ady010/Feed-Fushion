d817adafc42ee0e7fa3e287e3ee22f0fd87b1be9
Aditya Pahane <adityapahane@gmail.com>
2025-03-31 16:48:09 +0530

    Updating..

--- Backend/src/Controller/user.controller.js
+++ Backend/src/Controller/user.controller.js
@@ -1,3 +1,3 @@
 module.exports.register = ()=>{
-    console.log("create account")
+    const {name, email, password } = req.body()
 }
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,9 @@ const app = express()
 const Routes = require("../src/Routes/index.routes")
 const cors = require("cors")
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use(cors({origin:"http://localhost:5173"},{Credantial:true}))
 
 app.use("/",Routes)
