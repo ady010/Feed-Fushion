d2d91a80490b53448002f7a308a6a8690587bf95
Aditya Pahane <adityapahane@gmail.com>
2025-03-31 15:51:26 +0530

    Updating...

---
+++ Backend/src/Middleware/user.middleware.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("../Models/user.model")
+
+
+module.exprts.authUser = ()=>{
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+    }
+    catch(err){
+
+    }
+}
