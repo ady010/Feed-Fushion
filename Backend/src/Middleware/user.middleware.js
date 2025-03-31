6bb6f2d1a4c8a49bdff7072aea9385aebb9f4c3b
Aditya Pahane <adityapahane@gmail.com>
2025-03-31 16:03:45 +0530

    Updating...

--- Backend/src/Middleware/user.middleware.js
+++ Backend/src/Middleware/user.middleware.js
@@ -2,15 +2,21 @@ const jwt = require("jsonwebtoken")
 const userModel = require("../Models/user.model")
 
 
-module.exprts.authUser = ()=>{
+module.exprts.authUser = async (req, res, next)=>{
     try {
         const token = req.headers.authorization.split(" ")[1]
         if(!token){
             res.status(400).json({message:'Unauthorized'})
         }
-        
-    }
-    catch(err){
+        const decoded = userModel.verifyToken(token)
+        const user = await userModel.findOne({_id: decoded.id})
+
+        req.user = user
+
+        next()
 
     }
-}
+    catch(err){
+        res.status(400).json({message:'Unathorized'})
+    }
+} 
