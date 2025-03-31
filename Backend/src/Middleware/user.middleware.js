00198e4989e694c1afa47eac0dfb3312cf9910a9
Aditya Pahane <adityapahane@gmail.com>
2025-03-31 15:53:42 +0530

    Updating...

--- Backend/src/Middleware/user.middleware.js
+++ Backend/src/Middleware/user.middleware.js
@@ -5,6 +5,10 @@ const userModel = require("../Models/user.model")
 module.exprts.authUser = ()=>{
     try {
         const token = req.headers.authorization.split(" ")[1]
+        if(!token){
+            res.status(400).json({message:'Unauthorized'})
+        }
+        
     }
     catch(err){
 
