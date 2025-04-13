90fb608b3086390b9f1923279acc3b1efc4ce9a5
Aditya Pahane <adityapahane@gmail.com>
2025-04-13 23:52:29 +0530

    CreateBlog Update

--- Frontend/vite-project/src/views/Createblog.jsx
+++ Frontend/vite-project/src/views/Createblog.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const Createblog = () => {
+
+  const [image, setImage] = useState("")
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+  }
   return (
     <main>
         <form >
-            <input type="text" placeholder='Image' />
-            <input type="text" placeholder='Title'/>
-            <input type="text" placeholder='Content'/>
-            <button>Create</button>
+            <input type="text" value={image} onChange={(e)=>setImage(e.target.value)} placeholder='Image' />
+            <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} placeholder='Title'/>
+            <input type="text" value={content} onChange={(e)=>setContent(e.target.value)} placeholder='Content'/>
+            <button type='submit' onClick={submitHandler}>Create</button>
         </form>
     </main>
   )
